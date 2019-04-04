1e95cef381538670a53d0d63d0c4e391ada44c5a
伟伟权 <qww.zone@aliyun.com>
2019-04-04 10:47:46 +0800

    Create zh-cn.js
    I add the chinese language, can it used?

---
+++ plugins/emoji/lang/zh-cn.js
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.plugins.setLang( 'emoji', 'zh-cn', {
+	searchPlaceholder: '搜索emoji表情…',
+	searchLabel: '输入字段在面板内搜索和过滤表情符号。',
+	navigationLabel: '表情符号分组导航。',
+	title: 'Emoji表情列表',
+	groups: {
+		people: '人物',
+		nature: '自然和动物',
+		food: '食物和饮料',
+		travel: '旅行和景点',
+		activities: '活动',
+		objects: '事物',
+		symbols: '符号',
+		flags: '旗帜'
+	}
+} );
